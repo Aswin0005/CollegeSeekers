b9f12dce9a282213c675c2e786268f90c8273b4a
Aswin <aswinash19@gmail.com>
2024-11-10 11:38:47 +0530

    Fixex #1

--- src/app/components/LandingPageComponents/AdBlock4.jsx
+++ src/app/components/LandingPageComponents/AdBlock4.jsx
@@ -4,25 +4,18 @@ const AdBlock4 = () => {
   return (
     <div className="flex items-center bg-white p-2 drop-shadow-lg border rounded-md mt-10 w-[90%] mx-auto ">
       <div className="flex-shrink-0 relative">
-        <Image
-          src="/adblock4bg.png"
-          alt="Ad 4"
-          width={200}
-          height={200}
-         
-        />
+        <Image src="/adblock4bg.png" alt="Ad 4" width={200} height={200} />
         <Image
           src="/adblock4trophy.png"
           alt="Ad 4"
           width={90}
           height={90}
-         
           className="absolute top-1/2 -translate-y-1/2 left-[45%] -translate-x-1/2"
         />
       </div>
       <div className="ml-4 grow flex flex-col items-center">
         <h2 className="text-3xl font-bold text-center">
-          India's Most Convenient College Application Form
+          India&apos;s Most Convenient College Application Form
         </h2>
         <p className="text-gray-600 text-center">
           Apply to 2000+ colleges with just one form!
